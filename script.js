74c706d978ba147b4a5d7767f4a35115b9566664
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-31 02:41:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ urlUtils
 // If CSV files are not on the same domain as your website, a CORS enabled server
 // or a proxy is required.
 var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";
-esriConfig.request.corsEnabledServers.push('https://rawgit.com');
 
 // Paste the url into a browser's address bar to download and view the attributes
 // in the CSV file. These attributes include:
