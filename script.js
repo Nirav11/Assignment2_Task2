1763ce6a72723a07a74af87bb36506f48c0bb72e
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-31 03:02:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,81 @@
-require([
-"esri/Map",
-"esri/layers/CSVLayer",
-"esri/views/MapView",
-"esri/config",
-"esri/core/urlUtils",
-"dojo/domReady!"
-], function(
-Map,
-CSVLayer,
-MapView,
-esriConfig,
-urlUtils
-) {
+      require(["esri/WebScene", "esri/layers/CSVLayer", "esri/views/SceneView"], (
+        WebScene,
+        CSVLayer,
+        SceneView
+      ) => {
+        // If CSV files are not on the same domain as your website, a CORS enabled server
+        // or a proxy is required.
+        const url = "https://developers.arcgis.com/javascript/latest//sample-code/layers-csv/live/earthquakes.csv";
 
-// If CSV files are not on the same domain as your website, a CORS enabled server
-// or a proxy is required.
-var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";
+        // Paste the url into a browser's address bar to download and view the attributes
+        // in the CSV file. These attributes include:
+        // * mag - magnitude
+        // * type - earthquake or other event such as nuclear test
+        // * place - location of the event
+        // * time - the time of the event
 
-// Paste the url into a browser's address bar to download and view the attributes
-// in the CSV file. These attributes include:
-// * mag - magnitude
-// * type - earthquake or other event such as nuclear test
-// * place - location of the event
-// * time - the time of the event
+        const template = {
+          title: "Earthquake Info",
+          content: "Magnitude {mag} {type} hit {place} on {time}."
+        };
 
-const template = {
-  title: "Saint Louis Crime",
-  content: "Crime {Crime} at {ILEADSStreet} at {Longitude}, {Latitude}."
-};
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "USGS Earthquakes",
+          popupTemplate: template
+        });
 
-const csvLayer = new CSVLayer({
-  url: url,
-  copyright: "Crime at Saint Louis",
-  popupTemplate: template
-});
+        csvLayer.renderer = {
+          type: "simple", // autocasts as new SimpleRenderer()
+          symbol: {
+            type: "point-3d", // autocasts as new PointSymbol3D()
+            // for this symbol we use 2 symbol layers, one for the outer circle
+            // and one for the inner circle
+            symbolLayers: [{
+              type: "icon", // autocasts as new IconSymbol3DLayer()
+              resource: { primitive: "circle"},
+              material: { color: [255, 84, 54, 1] },
+              size: 5
+            }, {
+              type: "icon", // autocasts as new IconSymbol3DLayer()
+              resource: { primitive: "circle"},
+              material: { color: [255, 84, 54, 0] },
+              outline: {color: [255, 84, 54, 0.6], size: 1},
+              size: 25
+            }]
+          }
+        };
 
-var symbol = {
-  type: "simple-marker", 
-  color: "#add8e6"
-};
+        const map = new WebScene({
+          portalItem: {
+            id: "a467ef1140de4e88acf34d38df9fb869"
+          }
+        });
 
-csvLayer.renderer = {
-type: "simple", // autocasts as new SimpleRenderer()
-symbol: symbol
-};
+        map.add(csvLayer);
 
-var map = new Map({
-basemap: "gray",
-layers: [csvLayer]
-});
-
-var view = new MapView({
-container: "viewDiv",
-center: [-90.1994,38.6270,38.6270],
-zoom: 12,
-map: map
-});
-
-});
+        const view = new SceneView({
+          container: "viewDiv",
+          qualityProfile: "high",
+          map: map,
+          alphaCompositingEnabled: true,
+          highlightOptions: {
+            fillOpacity: 0,
+            color: "#ffffff"
+          },
+          constraints: {
+            altitude: {
+              min: 700000
+            }
+          },
+          environment: {
+            background: {
+              type: "color",
+              color: [0, 0, 0, 0]
+            },
+            lighting: {
+              type: "virtual"
+            }
+          }
+        });
+      });
